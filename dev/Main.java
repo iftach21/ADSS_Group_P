56b8b42e61971879d8bc392c099fd36bee307945
ofirtamir <ofirtam@post.bgu.ac.il>
2023-03-21 16:17:06 +0200

    heyyyyy

--- dev/Main.java
+++ dev/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("mabe now its working");
+        System.out.println("mabe now its working dfdfs");
     }
 }

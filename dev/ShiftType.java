57b2ad9fd1dc0ddcf38fb5eb4a5005823fc2078f
iftach21 <127610421+iftach21@users.noreply.github.com>
2023-03-21 16:28:46 +0200

    statingggg =)

---
+++ dev/ShiftType.java
@@ -0,0 +1,4 @@
+public enum ShiftType {
+    nightShift,
+    dayShift;
+}
---
+++ dev/Workers.java
@@ -0,0 +1,7 @@
+public class Workers {
+    private int id;
+    private String name;
+    private String contract;
+    private String start_date;
+    private
+}

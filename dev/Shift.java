e11f85d1ded7cbd9a2d757e11b7453e33b064be2
ofirtamir <ofirtam@post.bgu.ac.il>
2023-03-21 16:39:03 +0200

    21/03/23

---
+++ dev/Shift.java
@@ -0,0 +1,55 @@
+public class Shift {
+    private String date;
+    private int day;
+    private ShiftType shift;
+    private Workers[] workers;
+    private ShiftManager shiftManager;
+
+    public Shift(String date, int day, ShiftType shift, Workers[] workers, ShiftManager shiftManager) {
+        this.date = date;
+        this.day = day;
+        this.shift = shift;
+        this.workers = workers;
+        this.shiftManager = shiftManager;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public ShiftType getShift() {
+        return shift;
+    }
+
+    public void setShift(ShiftType shift) {
+        this.shift = shift;
+    }
+
+    public Workers[] getWorkers() {
+        return workers;
+    }
+
+    public void setWorkers(Workers[] workers) {
+        this.workers = workers;
+    }
+
+    public ShiftManager getShiftManager() {
+        return shiftManager;
+    }
+
+    public void setShiftManager(ShiftManager shiftManager) {
+        this.shiftManager = shiftManager;
+    }
+}

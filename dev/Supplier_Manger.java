dad86801b96e761fa2f47d7a0362dd5e06d37e2b
omer12619 <110417927+omer12619@users.noreply.github.com>
2023-03-28 20:02:10 +0300

    df

--- dev/Supplier_Manger.java
+++ dev/Supplier_Manger.java
@@ -1,4 +1,17 @@
+//This is the class that hold all the suppliers that work with the company
 public class Supplier_Manger {
+    public Supplier[] getSuppliers() {
+        return suppliers;
+    }
+
+    public void setSuppliers(Supplier[] suppliers) {
+        this.suppliers = suppliers;
+    }
+
+
     Supplier[] suppliers;
 
+    public Supplier_Manger(Supplier[] suppliers) {
+        this.suppliers = suppliers;
+    }
 }

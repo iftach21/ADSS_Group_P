bde1f2f7cada740af57e5064b75301ab3d67b253
omer12619 <110417927+omer12619@users.noreply.github.com>
2023-03-28 20:22:27 +0300

    df

--- dev/NonDeliveringSupplier.java
+++ dev/NonDeliveringSupplier.java
@@ -1,4 +1,6 @@
 public class NonDeliveringSupplier extends Supplier{
-    public NonDeliveringSupplier() {
+
+    public NonDeliveringSupplier(String name, String business_id, String payment_method, String suplier_ID, Contact_Person person, Contract contract) {
+        super(name, business_id, payment_method, suplier_ID, person, contract);
     }
 }

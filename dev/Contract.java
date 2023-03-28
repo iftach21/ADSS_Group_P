c5dcbc67f7048833d4a787d164e80d4bef0222dd
marikf98 <marikf98@gmail.com>
2023-03-28 19:35:42 +0300

    day

--- dev/Contract.java
+++ dev/Contract.java
@@ -3,5 +3,33 @@ public class Contract {
     Boolean Fulfilled;
     Boolean Fully_Fullfleld;
 
+    public String getOrder_num() {
+        return Order_num;
+    }
 
+    public void setOrder_num(String order_num) {
+        Order_num = order_num;
+    }
+
+    public Boolean getFulfilled() {
+        return Fulfilled;
+    }
+
+    public void setFulfilled(Boolean fulfilled) {
+        Fulfilled = fulfilled;
+    }
+
+    public Boolean getFully_Fullfleld() {
+        return Fully_Fullfleld;
+    }
+
+    public void setFully_Fullfleld(Boolean fully_Fullfleld) {
+        Fully_Fullfleld = fully_Fullfleld;
+    }
+
+    public Contract(String order_num, Boolean fulfilled, Boolean fully_Fullfleld) {
+        Order_num = order_num;
+        Fulfilled = fulfilled;
+        Fully_Fullfleld = fully_Fullfleld;
+    }
 }

aad68761967614de031a12c41e0b4204ab394cdd
omer12619 <110417927+omer12619@users.noreply.github.com>
2023-03-28 20:00:08 +0300

    df

--- dev/Supplier.java
+++ dev/Supplier.java
@@ -5,4 +5,61 @@ public abstract class Supplier {
     private String Suplier_ID;
     private Contact_Person person;
     private Contract contract;
+
+    public Supplier(String name, String business_id, String payment_method, String suplier_ID, Contact_Person person, Contract contract) {
+        this.name = name;
+        this.business_id = business_id;
+        Payment_method = payment_method;
+        Suplier_ID = suplier_ID;
+        this.person = person;
+        this.contract = contract;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBusiness_id() {
+        return business_id;
+    }
+
+    public void setBusiness_id(String business_id) {
+        this.business_id = business_id;
+    }
+
+    public String getPayment_method() {
+        return Payment_method;
+    }
+
+    public void setPayment_method(String payment_method) {
+        Payment_method = payment_method;
+    }
+
+    public String getSuplier_ID() {
+        return Suplier_ID;
+    }
+
+    public void setSuplier_ID(String suplier_ID) {
+        Suplier_ID = suplier_ID;
+    }
+
+    public Contact_Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Contact_Person person) {
+        this.person = person;
+    }
+
+    public Contract getContract() {
+        return contract;
+    }
+
+    public void setContract(Contract contract) {
+        this.contract = contract;
+    }
 }
---
+++ dev/Supplier_Manger.java
@@ -0,0 +1,4 @@
+public class Supplier_Manger {
+    Supplier[] suppliers;
+
+}

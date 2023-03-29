509a18b0161a50b0b36b90270d30d7abc8b8211b
omer12619 <110417927+omer12619@users.noreply.github.com>
2023-03-29 17:40:22 +0300

    CHANGE IN THE CONSTARCTOR OF ALL SUPPLIERS

--- dev/Order.java
+++ dev/Order.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -5,48 +6,71 @@ public class Order {
 
 
     private String orderNum;
-    private Map<Item,Integer> itemList;
+    private Map<Item,Pair<Integer,Supplier>> itemList;
 
     private Boolean fulfilled;
 
-    public Order(String orderNum, Map<Item, Integer> itemList, Boolean fulfilled) {
+
+    public Order(String orderNum, Map<Item, Pair<Integer, Supplier>> itemList, Boolean fulfilled) {
         this.orderNum = orderNum;
+        if(itemList==null){
+            this.itemList= new HashMap<Item,Pair<Integer,Supplier>>();
+        }
+        else {
+            this.itemList= itemList;
+        }
         this.itemList = itemList;
         this.fulfilled = fulfilled;
+
     }
 
-    public String getOrderNum() {
-        return orderNum;
-    }
-
-    public void setOrderNum(String orderNum) {
-        this.orderNum = orderNum;
-    }
-
-    public Map<Item, Integer> getItemList() {
-        return itemList;
-    }
-
-    public void setItemList(Map<Item, Integer> itemList) {
-        this.itemList = itemList;
-    }
-
-    public Boolean getFulfilled() {
-        return fulfilled;
-    }
-
-    public void setFulfilled(Boolean fulfilled) {
-        this.fulfilled = fulfilled;
-    }
     //This fuction is to remove an item to the order
     public void remove_item(Item item){
         this.itemList.remove(item);
 
     }
-    //This fuction is to remove an item from a order
-    public  void add_item(Item item, int amount){
-        this.itemList.put(item,amount);
+    public void add_item_to_Order(Item item,int amount,Supplier_Manger manger){
+        if(manger == null){
+            return;
+        }
+        float min_cost_for_amount_found=1000000000;
+        Supplier min_sup=null;
+        ///check who is the suppler  that sell the lowest cost
+        for(Supplier sup :manger.suppliers){
+            float cost=0;
+            //if he has the item
+            if(sup.getItems().containsKey(item)){
+                //cheak if he have a discount on it
+                float cost_of_item= sup.getItems().get(item);
+                int amount_on_discount=0;
+                if(sup.getContract().items_Map_discount.containsKey(item)){
+                    for(int i=0;i<amount;i++){
+                        if(sup.getContract().items_Map_discount.get(item).containsKey(amount -i)){
+                            amount_on_discount =amount-i;
+                            cost= (float) (cost_of_item*amount_on_discount*sup.getContract().items_Map_discount.get(item).get(amount -i));
+                                    break;
+                        }
+                    }
 
+
+
+                }
+                cost+=amount-amount_on_discount*cost_of_item;
+                if(cost<min_cost_for_amount_found){
+                    min_sup=sup;
+                    min_cost_for_amount_found=cost;
+                }
+
+            }
+        }
+        if(min_sup!=null){
+            Pair<Integer, Supplier> pair=new Pair<>(amount,min_sup);
+            this.itemList.put(item,pair);
+            return;
+        }
     }
 
+
 }
+
+

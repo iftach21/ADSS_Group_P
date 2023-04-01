166422938a07c03b50f032923fabaebe20f5315f
Ofir Horn <127858720+OfirHorn@users.noreply.github.com>
2023-04-01 17:38:53 +0300

    Update Main.java

--- dev/Main.java
+++ dev/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Interface in = new Interface();
+        in.logIn();
     }
 }

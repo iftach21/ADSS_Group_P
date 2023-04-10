7aeb553fc7ac43e8fde3d132f8b1f963547aad25
Ofir Horn <ofirhor@post.bgu.ac.il>
2023-04-10 11:02:17 +0300

    categoryname in Item

--- dev/ItemController.java
+++ dev/ItemController.java
@@ -8,9 +8,9 @@ public class ItemController {
         this.ItemList = new ArrayList<Item>();
     }
 
-    public Item addItem(String name, String catalogNum, double weight, String manufacturer, TempLevel temperature, int minQuantity)
+    public Item addItem(String name, String catalogNum, double weight, String manufacturer, TempLevel temperature, int minQuantity, String CategoryName)
     {
-        Item _item = new Item(name, catalogNum, weight, manufacturer, temperature, minQuantity);
+        Item _item = new Item(name, catalogNum, weight, manufacturer, temperature, minQuantity, CategoryName);
         ItemList.add(_item);
         return _item;
     }

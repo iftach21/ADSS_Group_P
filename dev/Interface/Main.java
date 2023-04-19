3a40bfc4821f4137af5d3d1419161aa3d5bc611c
iftach21 <127610421+iftach21@users.noreply.github.com>
2023-04-19 11:45:53 +0300

    finished the main menu just need to add shipment interface

--- dev/Interface/Main.java
+++ dev/Interface/Main.java
@@ -1,11 +1,50 @@
 package Interface;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    static final List<AInterface> intefaceList = new ArrayList<>(){{
+        add(bossInterface);
+        add(workerInterface);
+        //add(transferInterface) //todo: complete later;
+    }};
     static BossInterface bossInterface;
     static WorkerInterface workerInterface;
     //TODO: adding here the shipment interface:
     //static TransferInterface transferInterface;
     public static void main(String[] args) {
-        //todo: options to choose as what to loginto:
+        int option = 1;
+        while(option != 0){
+            System.out.println("What system would you like to log into?");
+            System.out.println("1 = HR Manager's");
+            System.out.println("2 = Employee's");
+            System.out.println("3 = Transfer Manager's");
+            System.out.println("0 = Exit system");
+
+
+            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+            option = myObj.nextInt();  // Read user input
+
+            //checking if the option works
+            if(option>3 || option < 0){
+                System.out.println("Invalid Input");
+                continue;
+            }
+
+
+            try{
+                intefaceList.get(option -1).logIn();
+          }
+            catch (ExitExeption e){
+                   continue;
+                 }
+
+             catch (Exception e){
+                System.out.println("Seems to be a problem, please contact IT");
+            }
+        }
+
     }
 }

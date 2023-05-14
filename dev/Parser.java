fb2b7991f7840a49f42d0f09f081c75020ab5517
omer12619 <110417927+omer12619@users.noreply.github.com>
2023-05-14 17:48:15 +0300

    updated ReportMapper

--- dev/Parser.java
+++ dev/Parser.java
@@ -3,8 +3,8 @@ import java.util.Map;
 import java.util.Objects;
 
 public class Parser {
-    public static Map<Item, Pair<Integer, Float>> parse(String input) {
-        Map<Item, Pair<Integer, Float>> map = new HashMap<>();
+    public static Map<String, Pair<Integer, Float>> parse(String input) {
+        Map<String, Pair<Integer, Float>> map = new HashMap<>();
         if(Objects.equals(input, "{}"))
         {
             return map;
@@ -13,51 +13,12 @@ public class Parser {
 
         for (String pair : pairs) {
             String[] parts = pair.split(":\\{");
-            Item item = getItem(parts[0]);
+            String item =parts[0];
             Pair<Integer, Float> pairValues = getPair(parts[1]);
             map.put(item, pairValues);
         }
 
         return map;
-    }
-
-    private static Item getItem(String input) {
-        String[] parts = input.split(",");
-        Item item = new Item();
-        for (String part : parts) {
-            String[] kv = part.split("=");
-            String key = kv[0].replaceAll("[{}\"' ]", "");
-            String value = kv[1].replaceAll("[{}\"' ]", "");
-            switch (key) {
-                case "Itemname":
-                    item.setName(value);
-                    break;
-                case "catalogNum":
-                    item.setCatalogNum(value);
-                    break;
-                case "weight":
-                    item.setWeight(Float.parseFloat(value));
-                    break;
-                case "catalogName":
-                    item.setCatalogName(value);
-                    break;
-                case "temperature":
-                    item.setTemperature(TempLevel.valueOf(value));
-                    break;
-                case "manufacturer":
-                    item.setManufacturer(value);
-                    break;
-                case "minimum_quantity":
-                    item.setMinimum_quantity(Integer.parseInt(value));
-                    break;
-                case "priceHistory":
-                    // ignore this field
-                    break;
-                default:
-                    throw new IllegalArgumentException("Invalid key: " + key);
-            }
-        }
-        return item;
     }
 
     private static Pair<Integer, Float> getPair(String input) {

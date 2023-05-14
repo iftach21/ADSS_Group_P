30a92d7b276ecfd6d4d7b46eddda67498705a57d
assayagt <assayagt@post.bgu.ac.il>
2023-05-14 18:32:06 +0300

    test changes

--- Test/DAOTest/IntegrationBetweenEmployeeAndTransfer.java
+++ Test/DAOTest/IntegrationBetweenEmployeeAndTransfer.java
@@ -5,6 +5,7 @@ import Domain.Employee.*;
 import Domain.Enums.TempLevel;
 import Domain.Enums.WindowType;
 import Domain.Enums.weightType;
+import Domain.Transfer.Transfer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -28,7 +29,7 @@ public class IntegrationBetweenEmployeeAndTransfer {
     WeeklyShiftAndWorkersManager wscontroller = WeeklyShiftAndWorkersManager.getInstance();
 
     List<Driver> drivers;
-    DriverController driverControllerTest;
+    DriverController driverControllerTest = DriverController.getInstance();
 
     public IntegrationBetweenEmployeeAndTransfer() throws SQLException {
     }
@@ -52,45 +53,68 @@ public class IntegrationBetweenEmployeeAndTransfer {
         wscontroller.addemployee(666,"iftach","lotsofmoney", "23.2.23",90,12345,"student",1234);
 
         //creating weeklyshift for the stokes:
-        wscontroller.createnewweeklyshift(11,1999,12);
+        wscontroller.createnewweeklyshift(1,2024,4);
 
-        wscontroller.createnewweeklyshift(11,1999,0);
+        wscontroller.createnewweeklyshift(1,2024,5);
+
+        wscontroller.createnewweeklyshift(1,2024,0);
 
 
         // all of the data is for week num 1 and year 1:
 
         //stokes will be available for day1, day 2 on day shift:
-        wscontroller.addtoexistingweeklyshift(11,1999,12, WindowType.day1,555,2);
-        wscontroller.addtoexistingweeklyshift(11,1999,12,WindowType.day1,666,2);
+        wscontroller.addtoexistingweeklyshift(1,2024,4, WindowType.day1,555,2);
+        wscontroller.addtoexistingweeklyshift(1,2024,4,WindowType.day1,666,2);
 
-        wscontroller.addtoexistingweeklyshift(11,1999,12, WindowType.day3,555,2);
-        wscontroller.addtoexistingweeklyshift(11,1999,12,WindowType.day3,666,2);
+        wscontroller.addtoexistingweeklyshift(1,2024,4, WindowType.day2,555,2);
+        wscontroller.addtoexistingweeklyshift(1,2024,4,WindowType.day2,666,2);
+
+        wscontroller.addtoexistingweeklyshift(1,2024,4, WindowType.day3,555,2);
+        wscontroller.addtoexistingweeklyshift(1,2024,4,WindowType.day3,666,2);
+
+        wscontroller.addtoexistingweeklyshift(1,2024,4, WindowType.day4,555,2);
+        wscontroller.addtoexistingweeklyshift(1,2024,4,WindowType.day4,666,2);
+
+        wscontroller.addtoexistingweeklyshift(1,2024,4, WindowType.day5,555,2);
+        wscontroller.addtoexistingweeklyshift(1,2024,4,WindowType.day5,666,2);
+
+        wscontroller.addtoexistingweeklyshift(1,2024,4, WindowType.day6,555,2);
+        wscontroller.addtoexistingweeklyshift(1,2024,4,WindowType.day6,666,2);
+
+        wscontroller.addtoexistingweeklyshift(1,2024,4, WindowType.day7,555,2);
+        wscontroller.addtoexistingweeklyshift(1,2024,4,WindowType.day7,666,2);
+
+        wscontroller.addtoexistingweeklyshift(1,2024,5, WindowType.day3,555,2);
+        wscontroller.addtoexistingweeklyshift(1,2024,5,WindowType.day3,666,2);
+
+        wscontroller.addtoexistingweeklyshift(1,2024,5, WindowType.day4,555,2);
+        wscontroller.addtoexistingweeklyshift(1,2024,5,WindowType.day4,666,2);
 
         //available for drivers:
         //for day 3: all of them
-        wscontroller.addtoexistingweeklyshift(11,1999,0, WindowType.day3,111,7);
-        wscontroller.addtoexistingweeklyshift(11,1999,0, WindowType.day3,222,7);
-        wscontroller.addtoexistingweeklyshift(11,1999,0, WindowType.day3,333,7);
-        wscontroller.addtoexistingweeklyshift(11,1999,0, WindowType.day3,444,7);
+        wscontroller.addtoexistingweeklyshift(1,2024,0, WindowType.day3,111,7);
+        wscontroller.addtoexistingweeklyshift(1,2024,0, WindowType.day3,222,7);
+        wscontroller.addtoexistingweeklyshift(1,2024,0, WindowType.day3,333,7);
+        wscontroller.addtoexistingweeklyshift(1,2024,0, WindowType.day3,444,7);
 
         // for day 1 just driver 1 and 2.
-        wscontroller.addtoexistingweeklyshift(11,1999,0, WindowType.day1,111,7);
-        wscontroller.addtoexistingweeklyshift(11,1999,0, WindowType.day1,222,7);
+        wscontroller.addtoexistingweeklyshift(1,2024,0, WindowType.day1,111,7);
+        wscontroller.addtoexistingweeklyshift(1,2024,0, WindowType.day1,222,7);
 
     }
 
 
     @AfterEach
     void tearDown() {
-
-        Connection.DeleteRows();
+       //Connection.DeleteRows();
     }
 
 
     @Test
-    void getAvailableDriversTest()
-    {
-        //todo: complete
+    void getAvailableDriversTest() throws SQLException {
+        //Connection.databaseReboot();
+        //List<Driver> driversForDay1 = driverControllerTest.getAvailableDrivers(11, 1999, WindowType.day1);
+        //List<Integer> IdsListFromTrucksTable1 = transferTrucksDAO.get(transfer1.getTruckLicenseNumber()).stream().map(Transfer::getTransferId).toList();
     }
 
     @Test
@@ -103,6 +127,11 @@ public class IntegrationBetweenEmployeeAndTransfer {
     void findDriverTest2()
     {
         //todo: complete
+    }
+
+    void doIHaveDriver()
+    {
+        //wscontroller.doIHaveStokeForTheShipment()
     }
 
 }

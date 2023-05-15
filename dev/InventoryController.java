70bd1765684023aa2cda11c340f3645d6b6499b3
Ofir Horn <ofirhor@post.bgu.ac.il>
2023-05-15 19:41:38 +0300

    testReportTypeByReportNum() fix

--- dev/InventoryController.java
+++ dev/InventoryController.java
@@ -426,11 +426,11 @@ public class InventoryController {
                 }
             }
             currentReport.addReportItem(item, defectiveAmount);
-            try {
-                this.reportMapper.insert(currentReport);
-            } catch (SQLException var8) {
-                throw new RuntimeException(var8);
-            }
+        }
+        try {
+            this.reportMapper.insert(currentReport);
+        } catch (SQLException var8) {
+            throw new RuntimeException(var8);
         }
         return currentReport;
     }

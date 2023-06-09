a9fc4473811aa69faeb9ed0b3654c83473d276cd
ofirtamir <ofirtam@post.bgu.ac.il>
2023-06-09 11:51:13 +0300

    ofir

--- dev/Interface/UI/UIHRManager.java
+++ dev/Interface/UI/UIHRManager.java
@@ -72,7 +72,7 @@ public class UIHRManager {
         changeWageForEmployee.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //todo: change wage *************************************************
+                showAddwagetoemployeeDialog();
             }
         });
         employeesMenu.add(changeWageForEmployee);
@@ -232,7 +232,175 @@ public class UIHRManager {
         HRManagerService.addemployee(id,name,contract,startDate,wage,phoneNum,personalInfo,bankNum);
     }
 }
+    private void showAddwagetoemployeeDialog() {
+        JTextField idField = new JTextField();
+        JTextField wageField = new JTextField();
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(new JLabel("ID:"));
+        panel.add(idField);
+        panel.add(new JLabel("Wage:"));
+        panel.add(wageField);
+        panel.add(new JLabel("Phone Number:"));
 
+        int result = JOptionPane.showConfirmDialog(null, panel, "add wage to employee", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+            // Get the values from the text fields
+            int id = Integer.parseInt(idField.getText());
+            int wage = Integer.parseInt(wageField.getText());
+            HRManagerService.addwagetoemployee(id,wage);
+        }
+    }
+    private void showChangeemployeecontractDialog() {
+        JTextField idField = new JTextField();
+        JTextField contractField = new JTextField();
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(new JLabel("ID:"));
+        panel.add(idField);
+        panel.add(new JLabel("Contract:"));
+        panel.add(contractField);
+        panel.add(new JLabel("Start Date:"));
+        int result = JOptionPane.showConfirmDialog(null, panel, "Change Employee Contract", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+            // Get the values from the text fields
+            int id = Integer.parseInt(idField.getText());
+            String contract = contractField.getText();
+            // Call the addEmployee function with the gathered input
+            HRManagerService.changeemployeecontract(id,contract);
+        }
+    }
+    private void showUpdateemployeesbankDialog() {
+        JTextField idField = new JTextField();
+        JTextField bankNumField = new JTextField();
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(new JLabel("ID:"));
+        panel.add(idField);
+        panel.add(new JLabel("Bank Number:"));
+        panel.add(bankNumField);
+
+        int result = JOptionPane.showConfirmDialog(null, panel, "Add Update Employee Bank num", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+            // Get the values from the text fields
+            int id = Integer.parseInt(idField.getText());
+            int bankNum = Integer.parseInt(bankNumField.getText());
+            // Call the addEmployee function with the gathered input
+            HRManagerService.updateemployeesbank(id,bankNum);
+        }
+    }
+    private void showAddavilableforemployeeDialog() {
+        JTextField idField = new JTextField();
+
+        JTextField daynumField = new JTextField();
+        JTextField nordField = new JTextField();
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(new JLabel("ID:"));
+        panel.add(idField);
+        panel.add(new JLabel("day num Info:"));
+        panel.add(daynumField);
+        panel.add(new JLabel("nord:"));
+        panel.add(nordField);
+
+        int result = JOptionPane.showConfirmDialog(null, panel, "Adda vilable for employee", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+            // Get the values from the text fields
+            int id = Integer.parseInt(idField.getText());
+            int daynum = Integer.parseInt(daynumField.getText());
+            String nordInfo = nordField.getText();
+
+            // Call the addEmployee function with the gathered input
+            HRManagerService.addavilableforemployee(id,daynum,nordInfo);
+        }
+    }
+    private void showAddnewproforemployeeDialog() {
+        JTextField idField = new JTextField();
+        JTextField profField = new JTextField();
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(new JLabel("ID:"));
+        panel.add(idField);
+        panel.add(new JLabel("Bank Number:"));
+        panel.add(profField);
+
+        int result = JOptionPane.showConfirmDialog(null, panel, "Add newpro for employee", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+            // Get the values from the text fields
+            int id = Integer.parseInt(idField.getText());
+            int prof = Integer.parseInt(profField.getText());
+            // Call the addEmployee function with the gathered input
+            HRManagerService.addnewproforemployee(id,prof);
+        }
+    }
+    private void showremoveprofforemployeeDialog() {
+        JTextField idField = new JTextField();
+        JTextField profField = new JTextField();
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(new JLabel("ID:"));
+        panel.add(idField);
+        panel.add(new JLabel("Bank Number:"));
+        panel.add(profField);
+
+        int result = JOptionPane.showConfirmDialog(null, panel, "remove pro for employee", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+            // Get the values from the text fields
+            int id = Integer.parseInt(idField.getText());
+            int prof = Integer.parseInt(profField.getText());
+            // Call the addEmployee function with the gathered input
+            HRManagerService.removeprofforemployee(id,prof);
+        }
+    }
+    private void showremoveavalbleforemployeeDialog() {
+        JTextField idField = new JTextField();
+        JTextField daynumField = new JTextField();
+        JTextField nordField = new JTextField();
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(new JLabel("ID:"));
+        panel.add(idField);
+        panel.add(new JLabel("day num:"));
+        panel.add(daynumField);
+        panel.add(new JLabel("nord:"));
+        panel.add(nordField);
+        int result = JOptionPane.showConfirmDialog(null, panel, "removeavalble for employee", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+            // Get the values from the text fields
+            int id = Integer.parseInt(idField.getText());
+            int daynum =  Integer.parseInt(daynumField.getText());
+            String nord = nordField.getText();
+            // Call the addEmployee function with the gathered input
+            HRManagerService.removeavalbleforemployee(id,daynum,nord);
+        }
+    }
+
+    private void showsetPersonalinfoDialog() {
+        JTextField idField = new JTextField();
+
+        JTextField persoinfoField = new JTextField();
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(new JLabel("ID:"));
+        panel.add(idField);
+
+        panel.add(new JLabel("persoinfo:"));
+        panel.add(persoinfoField);
+        int result = JOptionPane.showConfirmDialog(null, panel, "set Personal info", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+            // Get the values from the text fields
+            int id = Integer.parseInt(idField.getText());
+            String persoinfo = persoinfoField.getText();
+            // Call the addEmployee function with the gathered input
+            HRManagerService.setPersonalinfo(id,persoinfo);
+        }
+    }
     public static void main(String[] args) {
         SwingUtilities.invokeLater(UIHRManager::new);
     }

a0d14ee38d657686dab5d18fe3346159a2861655
omer12619 <110417927+omer12619@users.noreply.github.com>
2023-06-21 23:40:27 +0300

    CHANGE
    THIS IS THE CHANGE THAT MADE

--- dev/services/Supplier_Manger_GUI.java
+++ dev/services/Supplier_Manger_GUI.java
@@ -594,9 +594,10 @@ public class Supplier_Manger_GUI   implements ActionListener {
             String[] options = {"Regular", "Frozen", "Cold"};
             int selectedOption = JOptionPane.showOptionDialog(null, "Select an option:", "Temperature",
                     JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
-            String selectedOptionString = options[selectedOption].toLowerCase(); // Convert to lowercase
-            TempLevel tempLevel = TempLevel.valueOf(selectedOptionString);
 
+            String temp =String.valueOf(selectedOption);
+
+            TempLevel tempLevel = TempLevel.valueOf(temp);
 
             String manufacturer= JOptionPane.showInputDialog("manufacturer name:");
 
@@ -618,7 +619,7 @@ public class Supplier_Manger_GUI   implements ActionListener {
             float floatValue = 0;
 
             while (!validFloat) {
-                String input = JOptionPane.showInputDialog("Enter a float value for the cost per unit:");
+                String input = JOptionPane.showInputDialog("Enter a float value:");
 
                 try {
                     floatValue = Float.parseFloat(input);
